a0f21b646b9eeb949f1ffc11eca783e1a22fc78b
aliyeff-f <eliyevnurlan811@gmail.com>
2024-04-07 00:07:48 +0400

    main

--- Drew hays - Personal portfolio/assets/js/script.js
+++ Drew hays - Personal portfolio/assets/js/script.js
@@ -59,3 +59,57 @@ const header = document.querySelector("[data-header]");
 window.addEventListener("scroll", function () {
   header.classList[window.scrollY > 100 ? "add" : "remove"]("active");
 });
+
+const tiltElements = document.querySelectorAll("[data-tilt]");
+
+const initTilt = function (event) {
+
+  /** get tilt element center position */
+  const centerX = this.offsetWidth / 2;
+  const centerY = this.offsetHeight / 2;
+
+  const tiltPosY = ((event.offsetX - centerX) / centerX) * 10;
+  const tiltPosX = ((event.offsetY - centerY) / centerY) * 10;
+
+  this.style.transform = `perspective(1000px) rotateX(${tiltPosX}deg) rotateY(${tiltPosY - (tiltPosY * 2)}deg)`;
+
+}
+
+addEventOnElements(tiltElements, "mousemove", initTilt);
+
+addEventOnElements(tiltElements, "mouseout", function () {
+  this.style.transform = `perspective(1000px) rotateX(0deg) rotateY(0deg)`;
+});
+
+
+
+/**
+ * Tab content
+ */
+
+const tabBtns = document.querySelectorAll("[data-tab-btn]");
+const tabContents = document.querySelectorAll("[data-tab-content]");
+
+let lastActiveTabBtn = tabBtns[0];
+let lastActiveTabContent = tabContents[0];
+
+const filterContent = function () {
+
+  if (!(lastActiveTabBtn === this)) {
+
+    lastActiveTabBtn.classList.remove("active");
+    lastActiveTabContent.classList.remove("active");
+
+    this.classList.add("active");
+    lastActiveTabBtn = this;
+
+    const currentTabContent = document.querySelector(`[data-tab-content="${this.dataset.tabBtn}"]`);
+
+    currentTabContent.classList.add("active");
+    lastActiveTabContent = currentTabContent;
+
+  }
+
+}
+
+addEventOnElements(tabBtns, "click", filterContent);
